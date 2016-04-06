e41295ef13b4ef906ef7bef8de0e174d30a88f64
Eero Koivula <koivulae@gmail.com>
2016-04-06 18:40:19 +0300

    Conversation päivityksiä

--- Messages/src/Server/Conversation.java
+++ Messages/src/Server/Conversation.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class Conversation {
 
 	private List<Message> messages;
+	public List<Message> getMessages() {
+		return messages;
+	}
+	public void setMessages(Message message) {
+		this.messages.add(message);
+	}
 	public Conversation() {
 		// TODO Auto-generated constructor stub
 	}
